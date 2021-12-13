fee3ad33c63167e46dcaa02a4a724cc66ca9379c
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-13 14:52:57 -0500

    Stage 2 (Vertical Lines)

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -17,6 +17,18 @@ class MainWidget(Widget):
         # print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
         pass
 
+    def on_size(self, *args):
+        #print("On SIZE W:" + str(self.width) + " H:" + str(self.height))
+        #self.perspective_point_x = self.width/2
+        #self.perspective_point_y = self.height * 0.75
+        pass
+
+    def on_perspective_point_x(self, widget, value):
+        #print("PX: " + str(value))
+        pass
+    def on_perspective_point_y(self, widget, value):
+        #print("PY: " + str(value))
+        pass
 
 class GalaxyApp(App):
     pass
