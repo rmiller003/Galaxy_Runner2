b206f8a6c56e25488493d568f30dc52b0d01fa26
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-14 21:37:32 -0500

    Stage 3

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -3,15 +3,22 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Line
 
 
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
+    V_NB_LINES = 7
+    V_LINES_SPACING = .1 # percentage' in screen width
+    vertical_lines = []
+
     def __init__(self, **kwargs):
         super(MainWidget, self). __init__(**kwargs)
         # print("INIT W:" + str(self.width) + " H:" + str(self.height))
+        self.init_vertical_lines()
 
     def on_parent(self, widget, parent):
         # print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
@@ -21,7 +28,7 @@ class MainWidget(Widget):
         #print("On SIZE W:" + str(self.width) + " H:" + str(self.height))
         #self.perspective_point_x = self.width/2
         #self.perspective_point_y = self.height * 0.75
-        pass
+        self.update_vertical_lines()
 
     def on_perspective_point_x(self, widget, value):
         #print("PX: " + str(value))
@@ -29,6 +36,20 @@ class MainWidget(Widget):
     def on_perspective_point_y(self, widget, value):
         #print("PY: " + str(value))
         pass
+
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            #self.line = Line(points=[100, 0, 100])
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
+
+    def update_vertical_lines(self):
+        central_line_x = int(self.width / 2)
+        #self.line.points =[center_x, 0, center_x, 100]
+        for i in range(0, self.V_NB_LINES):
+            self.vertical_lines[i].points = []
+
 
 class GalaxyApp(App):
     pass
