7ff130616ef10b2758ce63caece46c4046b050a1
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-19 16:59:17 -0500

    Stage 5

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -51,9 +51,29 @@ class MainWidget(Widget):
         offset = -int(self.V_NB_LINES/2)
         for i in range(0, self.V_NB_LINES):
             line_x = int(central_line_x + offset*spacing)
-            self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
 
+    def transform(self, x, y):
+        #return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
+
+    def transform_2D(self, x, y):
+        return int(x), int(y)
+
+    def transform_perspective(self, x, y):
+        tr_y = y * self.perspective_point_y / self.height
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x-self.perspective_point_x
+        diff_y = self.perspective_point_y-tr_y
+        proportion_y = diff_y/self.perspective_point_y
+
+        tr_x = self.perspective_point_x +diff_x*proportion_y
+        return int(tr_x), int(tr_y)
 
 class GalaxyApp(App):
     pass
