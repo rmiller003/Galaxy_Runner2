0cb6c87fa4f6d83a16c09d8933426aacdd282a14
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-21 15:40:10 -0500

    Stage 7

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -2,7 +2,7 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, Clock
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
 
@@ -12,18 +12,21 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = .1 # percentage' in screen width
+    V_LINES_SPACING = .25 # percentage' in screen width
     vertical_lines = []
 
     H_NB_LINES = 15
-    H_LINES_SPACING = .2 # percentage' in screen height
+    H_LINES_SPACING = .1 # percentage' in screen height
     horizontal_lines = []
+
+    current_offset_y = 0
 
     def __init__(self, **kwargs):
         super(MainWidget, self). __init__(**kwargs)
         # print("INIT W:" + str(self.width) + " H:" + str(self.height))
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
 
     def on_parent(self, widget, parent):
         # print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
@@ -33,8 +36,9 @@ class MainWidget(Widget):
         #print("On SIZE W:" + str(self.width) + " H:" + str(self.height))
         #self.perspective_point_x = self.width/2
         #self.perspective_point_y = self.height * 0.75
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        #self.update_vertical_lines()
+        #self.update_horizontal_lines()
+        pass
 
     def on_perspective_point_x(self, widget, value):
         #print("PX: " + str(value))
@@ -80,7 +84,7 @@ class MainWidget(Widget):
         spacing_y = self.H_LINES_SPACING*self.height
 
         for i in range(0, self.H_NB_LINES):
-            line_y = i*spacing_y
+            line_y = i*spacing_y-self.current_offset_y
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
@@ -94,20 +98,27 @@ class MainWidget(Widget):
         return int(x), int(y)
 
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x-self.perspective_point_x
-        diff_y = self.perspective_point_y-tr_y
-        proportion_y = diff_y/self.perspective_point_y
+        diff_y = self.perspective_point_y-lin_y
+        factor_y = diff_y/self.perspective_point_y
+        factor_y = pow(factor_y, 4)
+        tr_x = self.perspective_point_x + diff_x*factor_y
+        tr_y = self.perspective_point_y - factor_y*self.perspective_point_y
 
-        tr_x = self.perspective_point_x +diff_x*proportion_y
         return int(tr_x), int(tr_y)
+
+    def update(self, dt):
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+        self.current_offset_y += 1
+
 
 class GalaxyApp(App):
     pass
-
 
 
 GalaxyApp().run()
