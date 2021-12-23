8dc3dfc5ab9464e44d39daaf22c720a4ea1fb46c
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-23 18:56:43 -0500

    Stage 10

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -14,6 +14,8 @@ from kivy.graphics.vertex_instructions import Line
 
 
 class MainWidget(Widget):
+    from transform import transform, transform_2D, transform_perspective
+    from user_actions import keyboard_closed, on_keyboard_up, on_keyboard_down, on_touch_up, on_touch_down
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
@@ -46,38 +48,10 @@ class MainWidget(Widget):
 
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
-    def keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self.on_keyboard_down)
-        #self._keyboard.unbind(on_key_up=self.on_keyboard_up)
-        self._keyboard = None
-
     def is_desktop(self):
         if platform in ('linux', 'windows', 'macosx'):
             return True
         return False
-
-    def on_keyboard_up(self, keyboard, keycode):
-        self.current_speed_x = 0
-        return True
-
-    def on_parent(self, widget, parent):
-        # print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
-        pass
-
-    def on_size(self, *args):
-        #print("On SIZE W:" + str(self.width) + " H:" + str(self.height))
-        #self.perspective_point_x = self.width/2
-        #self.perspective_point_y = self.height * 0.75
-        #self.update_vertical_lines()
-        #self.update_horizontal_lines()
-        pass
-
-    def on_perspective_point_x(self, widget, value):
-        #print("PX: " + str(value))
-        pass
-    def on_perspective_point_y(self, widget, value):
-        #print("PY: " + str(value))
-        pass
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -86,18 +60,21 @@ class MainWidget(Widget):
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
+    def get_line_x_from_index(self, index):
+        central_line_x = self.perspective_point_x
+        spacing =self.V_LINES_SPACING * self.width
+        offset = index - 0.5
+        line_x = central_line_x + offset*spacing + self.current_offset_x
+        return line_x
 
     def update_vertical_lines(self):
-        central_line_x = int(self.width / 2)
-        spacing =self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES/2)+0.5
-        for i in range(0, self.V_NB_LINES):
-            line_x = central_line_x + offset*spacing + self.current_offset_x
+        start_index = -int(self.V_NB_LINES/2)+1
+        for i in range(start_index, start_index+self.V_NB_LINES):
+            line_x = self.get_line_x_from_index(i)
 
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
-            offset += 1
 
     def init_horizontal_lines(self):
         with self.canvas:
@@ -108,12 +85,10 @@ class MainWidget(Widget):
 
 
     def update_horizontal_lines(self):
-        central_line_x = int(self.width / 2)
-        spacing =self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES/2)+0.5
-
-        xmin = central_line_x+offset*spacing + self.current_offset_x
-        xmax = central_line_x-offset*spacing + self.current_offset_x
+        start_index = -int(self.V_NB_LINES / 2) + 1
+        end_index = start_index+self.V_NB_LINES-1
+        xmin = self.get_line_x_from_index(start_index)
+        xmax = self.get_line_x_from_index(end_index)
         spacing_y = self.H_LINES_SPACING*self.height
 
         for i in range(0, self.H_NB_LINES):
@@ -122,44 +97,6 @@ class MainWidget(Widget):
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
-
-    def transform(self, x, y):
-        #return self.transform_2D(x, y)
-        return self.transform_perspective(x, y)
-
-    def transform_2D(self, x, y):
-        return int(x), int(y)
-
-    def transform_perspective(self, x, y):
-        lin_y = y * self.perspective_point_y / self.height
-        if lin_y > self.perspective_point_y:
-            lin_y = self.perspective_point_y
-
-        diff_x = x-self.perspective_point_x
-        diff_y = self.perspective_point_y-lin_y
-        factor_y = diff_y/self.perspective_point_y
-        factor_y = pow(factor_y, 4)
-        tr_x = self.perspective_point_x + diff_x*factor_y
-        tr_y = self.perspective_point_y - factor_y*self.perspective_point_y
-
-        return int(tr_x), int(tr_y)
-
-    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == 'left':
-            self.current_speed_x = self.SPEED_X
-        elif keycode[1] == 'right':
-            self.current_speed_x = -self.SPEED_X
-
-        return True
-
-    def on_touch_down(self, touch):
-        if touch.x < self.width/2:
-            self.current_speed_x = self.SPEED_X
-        else:
-            self.current_speed_x = -self.SPEED_X
-
-    def on_touch_up(self, touch):
-        self.current_speed_x = 0
 
     def update(self, dt):
         time_factor = dt*60
