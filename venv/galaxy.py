d711fb1830a936c71ed9d20a20ef264e1202566b
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-22 21:46:06 -0500

    Stage 8

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -1,5 +1,9 @@
 # This is an Advanced Galaxy Runner Desktop Kivy/Python App written in Python 3.9
 
+from kivy.config import Config
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, Clock
@@ -19,7 +23,11 @@ class MainWidget(Widget):
     H_LINES_SPACING = .1 # percentage' in screen height
     horizontal_lines = []
 
+    SPEED = 4
     current_offset_y = 0
+
+    SPEED_X =3
+    current_offset_x = 0
 
     def __init__(self, **kwargs):
         super(MainWidget, self). __init__(**kwargs)
@@ -60,7 +68,8 @@ class MainWidget(Widget):
         spacing =self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES/2)+0.5
         for i in range(0, self.V_NB_LINES):
-            line_x = int(central_line_x + offset*spacing)
+            line_x = central_line_x + offset*spacing + self.current_offset_x
+
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
@@ -79,8 +88,8 @@ class MainWidget(Widget):
         spacing =self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES/2)+0.5
 
-        xmin = central_line_x+offset*spacing
-        xmax = central_line_x-offset*spacing
+        xmin = central_line_x+offset*spacing + self.current_offset_x
+        xmax = central_line_x-offset*spacing + self.current_offset_x
         spacing_y = self.H_LINES_SPACING*self.height
 
         for i in range(0, self.H_NB_LINES):
@@ -112,9 +121,16 @@ class MainWidget(Widget):
         return int(tr_x), int(tr_y)
 
     def update(self, dt):
+        time_factor = dt*60
         self.update_vertical_lines()
         self.update_horizontal_lines()
-        self.current_offset_y += 1
+        self.current_offset_y += self.SPEED * time_factor
+
+        spacing_y = self.H_LINES_SPACING * self.height
+        if self.current_offset_y >= spacing_y:
+            self.current_offset_y -= spacing_y
+
+        self.current_offset_x += self.SPEED_X * time_factor
 
 
 class GalaxyApp(App):
