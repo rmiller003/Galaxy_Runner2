37d6ced95fc9f3b37d48cc845701409c73c665d0
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-12-29 22:47:48 -0500

    Stage 15

--- venv/galaxy.py
+++ venv/galaxy.py
@@ -1,6 +1,9 @@
 # This is an Advanced Galaxy Runner Desktop Kivy/Python App written in Python 3.9
 import random
 from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.relativelayout import RelativeLayout
+
 Config.set('graphics', 'width', '900')
 Config.set('graphics', 'height', '400')
 
@@ -9,30 +12,33 @@ from kivy.core.window import Window
 from kivy.app import App
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line, Quad, Triangle
-from kivy.properties import NumericProperty, Clock
+from kivy.properties import NumericProperty, Clock, ObjectProperty, StringProperty
 from kivy.uix.widget import Widget
 
+Builder.load_file("menu.kv")
 
-class MainWidget(Widget):
+
+class MainWidget(RelativeLayout):
     from transform import transform, transform_2D, transform_perspective
     from user_actions import keyboard_closed, on_keyboard_up, on_keyboard_down, on_touch_up, on_touch_down
+
+    menu_widget = ObjectProperty()
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-
     V_NB_LINES = 8
-    V_LINES_SPACING = .5 # percentage' in screen width
+    V_LINES_SPACING = .4  # percentage' in screen width
     vertical_lines = []
 
-    H_NB_LINES = 22
-    H_LINES_SPACING = .1 # percentage' in screen height
+    H_NB_LINES = 15
+    H_LINES_SPACING = .1  # percentage' in screen height
     horizontal_lines = []
 
-    SPEED = .5
+    SPEED = .8
     current_offset_y = 0
     current_y_loop = 0
 
-    SPEED_X = 1.0
+    SPEED_X = 6.0
     current_speed_x = 0
     current_offset_x = 0
 
@@ -46,9 +52,11 @@ class MainWidget(Widget):
     ship = None
     ship_coordinates = [(0, 0), (0, 0), (0, 0)]
 
+    state_game_over = False
+    state_game_has_started = False
 
     def __init__(self, **kwargs):
-        super(MainWidget, self) .__init__(**kwargs)
+        super(MainWidget, self).__init__(**kwargs)
         # print("INIT W:" + str(self.width) + " H:" + str(self.height))
         self.init_vertical_lines()
         self.init_horizontal_lines()
@@ -60,7 +68,7 @@ class MainWidget(Widget):
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
             self._keyboard.bind(on_key_down=self.on_keyboard_down)
-            #self._keyboard.bind(on_key_up=self.on_keyboard_down)
+            self._keyboard.bind(on_key_up=self.on_keyboard_up)
 
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
@@ -69,13 +77,13 @@ class MainWidget(Widget):
             return True
         return False
 
-    #def Image(self):
+    # def Image(self):
 
     def init_ship(self):
         with self.canvas:
             Color(0, 0, 0)
             self.ship = Triangle()
-            #self.ship = starship.png()
+            # self.ship = starship.png()
 
     def update_ship(self):
         center_x = self.width / 2
@@ -83,7 +91,7 @@ class MainWidget(Widget):
         ship_half_width = self.SHIP_WIDTH * self.width / 2
         ship_height = self.SHIP_HEIGHT * self.height
 
-        self.ship_coordinates[0] = (center_x-ship_half_width, base_y)
+        self.ship_coordinates[0] = (center_x - ship_half_width, base_y)
         self.ship_coordinates[1] = (center_x, base_y + ship_height)
         self.ship_coordinates[2] = (center_x + ship_half_width, base_y)
 
@@ -102,7 +110,6 @@ class MainWidget(Widget):
                 return True
         return False
 
-
     def check_ship_collision_with_tile(self, ti_x, ti_y):
         xmin, ymin = self.get_tile_coordinates(ti_x, ti_y)
         xmax, ymax = self.get_tile_coordinates(ti_x + 1, ti_y + 1)
@@ -112,7 +119,6 @@ class MainWidget(Widget):
                 return True
         return False
 
-
     def init_tiles(self):
         with self.canvas:
             Color(1, 1, 1)
@@ -120,7 +126,7 @@ class MainWidget(Widget):
                 self.tiles.append(Quad())
 
     def pre_fill_tiles_coordinates(self):
-        for i in range(0, 20):
+        for i in range(0, 10):
             self.tiles_coordinates.append((0, i))
 
     def generate_tiles_coordinates(self):
@@ -129,7 +135,7 @@ class MainWidget(Widget):
 
         # clean the coordinates that are out of the screen
         # ti_y < self.current_y_loop
-        for i in range(len(self.tiles_coordinates)-1, -1, -1):
+        for i in range(len(self.tiles_coordinates) - 1, -1, -1):
             if self.tiles_coordinates[i][1] < self.current_y_loop:
                 del self.tiles_coordinates[i]
 
@@ -171,7 +177,7 @@ class MainWidget(Widget):
     def init_vertical_lines(self):
         with self.canvas:
             Color(1, 1, 1)
-            #self.line = Line(points=[100, 0, 100])
+            # self.line = Line(points=[100, 0, 100])
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
@@ -179,12 +185,12 @@ class MainWidget(Widget):
         central_line_x = self.perspective_point_x
         spacing = self.V_LINES_SPACING * self.width
         offset = index - 0.5
-        line_x = central_line_x + offset*spacing + self.current_offset_x
+        line_x = central_line_x + offset * spacing + self.current_offset_x
         return line_x
 
     def get_line_y_from_index(self, index):
-        spacing_y = self.H_LINES_SPACING*self.height
-        line_y = index*spacing_y-self.current_offset_y
+        spacing_y = self.H_LINES_SPACING * self.height
+        line_y = index * spacing_y - self.current_offset_y
         return line_y
 
     def get_tile_coordinates(self, ti_x, ti_y):
@@ -198,7 +204,7 @@ class MainWidget(Widget):
             tile = self.tiles[i]
             tile_coordinates = self.tiles_coordinates[i]
             xmin, ymin = self.get_tile_coordinates(tile_coordinates[0], tile_coordinates[1])
-            xmax, ymax = self.get_tile_coordinates(tile_coordinates[0]+1, tile_coordinates[1]+1)
+            xmax, ymax = self.get_tile_coordinates(tile_coordinates[0] + 1, tile_coordinates[1] + 1)
 
             x1, y1 = self.transform(xmin, ymin)
             x2, y2 = self.transform(xmin, ymax)
@@ -207,10 +213,9 @@ class MainWidget(Widget):
 
             tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
 
-
     def update_vertical_lines(self):
-        start_index = -int(self.V_NB_LINES/2)+1
-        for i in range(start_index, start_index+self.V_NB_LINES):
+        start_index = -int(self.V_NB_LINES / 2) + 1
+        for i in range(start_index, start_index + self.V_NB_LINES):
             line_x = self.get_line_x_from_index(i)
 
             x1, y1 = self.transform(line_x, 0)
@@ -220,14 +225,13 @@ class MainWidget(Widget):
     def init_horizontal_lines(self):
         with self.canvas:
             Color(1, 1, 1)
-            #self.line = Line(points=[100, 0, 100])
+            # self.line = Line(points=[100, 0, 100])
             for i in range(0, self.H_NB_LINES):
                 self.horizontal_lines.append(Line())
 
-
     def update_horizontal_lines(self):
         start_index = -int(self.V_NB_LINES / 2) + 1
-        end_index = start_index+self.V_NB_LINES-1
+        end_index = start_index + self.V_NB_LINES - 1
 
         xmin = self.get_line_x_from_index(start_index)
         xmax = self.get_line_x_from_index(end_index)
@@ -238,28 +242,35 @@ class MainWidget(Widget):
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
-
     def update(self, dt):
-        time_factor = dt*60
+        time_factor = dt * 60
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
         self.update_ship()
 
-        speed_y = self.SPEED * self.height / 100
-        self.current_offset_y += speed_y * time_factor
+        if not self.state_game_over and self.state_game_has_started:
+            speed_y = self.SPEED * self.height / 100
+            self.current_offset_y += speed_y * time_factor
 
-        spacing_y = self.H_LINES_SPACING * self.height
-        if self.current_offset_y >= spacing_y:
-            self.current_offset_y -= spacing_y
-            self.current_y_loop += 1
-            self.generate_tiles_coordinates()
+            spacing_y = self.H_LINES_SPACING * self.height
+            while self.current_offset_y >= spacing_y:
+                self.current_offset_y -= spacing_y
+                self.current_y_loop += 1
+                self.generate_tiles_coordinates()
+                print("loop : " + str(self.current_y_loop))
 
-        speed_x = self.current_speed_x * self.width / 100
-        self.current_offset_x += speed_x * time_factor
+            speed_x = self.current_speed_x * self.width / 100
+            self.current_offset_x += speed_x * time_factor
 
-        if not self.check_ship_collision():
+        if not self.check_ship_collision() and not self.state_game_over:
+            self.state_game_over = True
+            self.menu_widget.opacity = 1
             print("GAME OVER")
+
+    def on_menu_button_pressed(self):
+        self.state_game_has_started = True
+        self.menu_widget.opacity = 0
 
 
 class GalaxyApp(App):
